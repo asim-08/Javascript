b65036e724a4a2bf313fad4ba21e8625ddfd4429
Asim <asimkhanwc786@gmail.com>
2026-07-28 18:33:55 +0500

    learning about data types and variable

---
+++ Data-types-variable.js
@@ -0,0 +1,9 @@
+/*
+Data Types:
+undefined, null, boolean, string, symbol, number, and object.
+ */
+//Methods for Decleration data types.
+console.log(myname)
+var myname = "Beauty";
+//let collegename = "Tevta college";
+//const pi = 3.14
