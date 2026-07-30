73a0d51acd09d3fb18580d51101fa4200e23a30a
Asim <asimkhanwc786@gmail.com>
2026-07-31 01:44:12 +0500

    Learning about conditionals statement

---
+++ conditional.js
@@ -0,0 +1,24 @@
+let age = 0;
+if (age >= 18) {
+    console.log("You can vote.")
+}
+else if (age < 18 && age > 0) {
+    console.log("You can not vote.")
+}
+else{
+    console.log("Enter correct age.")
+}
+
+let num = 0;
+if (num > 0) {
+    console.log("Positive")
+}
+else if (num < 0) {
+    console.log("Negetive")
+}
+else if (num === 0) {
+    console.log("Zero")
+}
+else{
+    "Enter valid number."
+}
