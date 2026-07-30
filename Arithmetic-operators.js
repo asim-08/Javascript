18928ca6ddb4a7989b36097e010748858a6bf23e
Asim <asimkhanwc786@gmail.com>
2026-07-30 19:32:38 +0500

    Learning about arithmetic operators

---
+++ Arithmetic-operators.js
@@ -0,0 +1,16 @@
+a = 10;
+let b = 13;
+let sum = a + b;
+let sub = b - a;
+let mul = a * b;
+let div = b / a;
+let inc = ++a;
+let dec = --b;
+let rem = b % a;
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(inc);
+console.log(dec);
+console.log(rem)
